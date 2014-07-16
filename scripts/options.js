519115b217795771b19b09560f697b578a5264b7
crimx <jaward@foxmail.com>
2014-07-17 01:29:49 +0800

    小失误，代码写到模块外部

--- scripts/options.js
+++ scripts/options.js
@@ -26,22 +26,27 @@
 			}
 		});
 	});
+
+	function saveChanges() {
+		var settings = $(".settings");
+		var data = {};
+		$(".settings").each(function() {
+			data[$(this).prop("id")] = $(this).prop("checked");
+		});
+		chrome.storage.local.set({
+			'settings': data
+		}, function() {
+			chrome.storage.sync.set({
+				'settings': data
+			});
+		});
+	}
+
+	function showSettings(data) {
+		for (var id in data) {
+			$("#" + id).prop("checked", data[id]);
+		}
+	}
+	
 })(window, jQuery);
 
-function saveChanges() {
-	var settings = $(".settings");
-	// settings.prop('checked', true);
-	var data = {};
-	$(".settings").each(function() {
-		data[$(this).prop("id")] = $(this).prop("checked");
-	});
-	chrome.storage.local.set({'settings': data}, function() {
-		chrome.storage.sync.set({'settings': data});
-	});
-}
-
-function showSettings(data) {
-	for(var id in data) {
-		$("#" + id).prop("checked", data[id]);
-	}
-}
